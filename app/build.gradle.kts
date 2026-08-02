84e8f68a758ed19ccf75b4ede3fbb1ed7374ef36
Jinsoo <42793920+jinsoo1@users.noreply.github.com>
2026-08-02 17:07:13 +0900

    chore: 버전업 1.0.8

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -29,8 +29,8 @@ android {
         applicationId = "com.jsworld.android.autolog"
         minSdk = 29
         targetSdk = 36
-        versionCode = 8
-        versionName = "1.0.7"
+        versionCode = 9
+        versionName = "1.0.8"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
